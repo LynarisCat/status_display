da4ebb1b9330e00dccd01c18b6aa5f60823f559d
Aris <paul.ferber@gmx.de>
2026-04-07 20:03:48 +0200

    changed convert.py serverside to take a PixelAccess as input

--- server/convert.py
+++ server/convert.py
@@ -1,41 +1,37 @@
 from PIL import Image
 
 
-def imgToByte(inpFile: str) -> bytearray:
+def imgToByte(pix: Image.core.PixelAccess) -> bytearray:
     by = bytearray()
     c = 0
     bit = ""
 
-    with Image.open(inpFile) as im:
-    
-        pix = im.load()
-
-        for y in range(im.size[1]):
-            for x in range(im.size[0]):
+    for y in range(im.size[1]):
+        for x in range(im.size[0]):
             
-                if c >= 4:
-                    c = 1
-                    by += int(bit, 2).to_bytes(len(bit) // 8, byteorder='big')
-                    bit = ""
+            if c >= 4:
+                c = 1
+                by += int(bit, 2).to_bytes(len(bit) // 8, byteorder='big')
+                bit = ""
 
-                else:
-                    c += 1
+            else:
+                c += 1
     
-                match pix[x,y]:
-                    case 0:
-                        bit += "00"
+            match pix[x,y]:
+                case 0:
+                    bit += "00"
 
-                    case 64:
-                        bit += "10"
+                case 64:
+                    bit += "10"
 
-                    case 192:
-                        bit += "01"
+                case 192:
+                    bit += "01"
     
-                    case 255:
-                        bit += "11"
+                case 255:
+                    bit += "11"
 
-                    case default:
-                        pass
+                case default:
+                    pass
 
     if bit:
         bit = bit.ljust(8, "0")
@@ -48,7 +44,9 @@ def imgToByte(inpFile: str) -> bytearray:
 
    
 if __name__ == "__main__":
-    by = imgToByte("status-disp-idea.png")
+
+    with Image.open("status-disp-idea.png") as im:
+        by = imgToByte(im.load())
 
     with open("status-disp-idea.bytes", "wb") as f:
         f.write(by)
